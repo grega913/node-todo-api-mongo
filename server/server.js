6a2877c22dcdb73109866fefe8b2d4ee6991a3a4
grega913 <grega913@gmail.com>
2017-12-06 09:52:11 +0100

    Add post route

--- server/server.js
+++ server/server.js
@@ -1,34 +1,26 @@
-var mongoose=require('mongoose')
+var express =require('express')
+var bodyParser = require('body-parser')
 
-mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp')
+var {mongoose} = require('./db/mongoose')
+var {Todo} = require('./models/todo')
+var {User} = require('./models/user')
 
-var Todo = mongoose.model('Todo', {
-    text: {
-        type:String
-    },
-    completed: {
-        type:Boolean
-    },
-    completedAt: {
-        type:Number
-    }
+var app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req,res) => {
+    var todo = new Todo({
+        text: req.body.text
+    })
+
+    todo.save().then((doc)=>{
+        res.send(doc)
+    }, (e)=> {
+        res.status(400).send(e)
+    })
 })
 
-var newTodo = new Todo({
-    text: 'Cook dinner'
-})
-
-
-var otherTodo = new Todo({
-    text: 'Feed the cat',
-    completed: true,
-    completedAt: 123
-})
-
-
-otherTodo.save().then((doc) => {
-    console.log(JSON.stringify(doc, undefined,2))
-}, (e) => {
-    console.log('Unable to save todo', e)
+app.listen(3000,()=>{
+    console.log('started on port 3000')
 })
