7a97a74886f05fc82a0515357cbd5225ea33c7c3
Grega S <grega913@gmail.com>
2017-12-09 14:44:36 +0100

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,17 @@
 const {SHA256} = require('crypto-js')
 const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
+var password = '123abc!'
+
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash)
+    })
+})
+
+
+/*
 var data = {
     id:10
 }
@@ -10,3 +21,4 @@ console.log(token)
 
 var decode = jwt.verify(token,'123abc')
 console.log(decode)
+*/
--- server/server.js
+++ server/server.js
@@ -8,14 +8,12 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
-var {authenticate} = require('./middleware/authenticate')
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
-
-
 
 app.post('/todos', (req, res) => {
   var todo = new Todo({
@@ -100,28 +98,22 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 // POST /users
-app.post('/users', (req, res) => { 
+app.post('/users', (req, res) => {
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
   user.save().then(() => {
-    return user.generateAuthToken()
+    return user.generateAuthToken();
   }).then((token) => {
-    res.header('x-auth', token).send(user)
+    res.header('x-auth', token).send(user);
   }).catch((e) => {
     res.status(400).send(e);
   })
 });
 
-
-
-app.get('/users/me', authenticate, (req, res)=>{
-    res.send(req.user)
-})
-
-
-
-
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
+});
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
