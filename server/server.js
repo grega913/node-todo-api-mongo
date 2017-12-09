cb9e02fcca6b88f48c3dfe00d5a76194cc0dd8ec
Grega S <grega913@gmail.com>
2017-12-09 14:10:03 +0100

    GET /users/me - authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate')
 
 var app = express();
 const port = process.env.PORT;
@@ -98,10 +99,8 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
-
-
 // POST /users
-app.post('/users', (req, res) => {
+app.post('/users', (req, res) => { 
   var body = _.pick(req.body, ['email', 'password']);
   var user = new User(body);
 
@@ -113,6 +112,15 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   })
 });
+
+
+
+app.get('/users/me', authenticate, (req, res)=>{
+    res.send(req.user)
+})
+
+
+
 
 
 app.listen(port, () => {
