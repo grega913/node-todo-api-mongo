b170ba71451f4bcb16cc458ff905b666e5fb2000
grega913 <grega913@gmail.com>
2017-12-18 10:27:56 +0100

    Add tests for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,3 +1,4 @@
+
 const expect = require('expect');
 const request = require('supertest');
 const {ObjectID} = require('mongodb');
@@ -297,3 +298,25 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token',()=> {
+  it('should remove auth token on logout', (done)=>{
+    //DELETE /users/me/token
+    //Set x-auth equal to token
+    //200
+    //Find user, verify that tokens array has length of zero
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth',users[0].tokens[0].token)
+    .expect(200)
+    .end((err,res)=> {
+      if(err) {
+        return done(err)
+      }
+      User.findById(users[0]._id).then((user)=> {
+        expect(user.tokens.length).toBe(0)
+        done()
+      }).catch ((e) => done(e))
+    })
+  })
+})
